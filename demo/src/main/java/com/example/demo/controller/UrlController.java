697a38dc9db283c6cba571cd552d8cdaaf37d338
Dattebayo <angcsherpa01@gmail.com>
2025-12-09 07:59:29 -0500

    Application is working correctly, working on resolving null value for url when getting them from database

--- demo/src/main/java/com/example/demo/controller/UrlController.java
+++ demo/src/main/java/com/example/demo/controller/UrlController.java
@@ -3,8 +3,12 @@ package com.example.demo.controller;
 import com.example.demo.entity.UrlRequest;
 import com.example.demo.repository.UrlRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class UrlController {
@@ -13,8 +17,14 @@ public class UrlController {
     private UrlRepository urlRepository;
 
     @PostMapping("/analyze")
-    public String postUrl( UrlRequest url) {
+    public String postUrl(@RequestBody UrlRequest url) {
+        UrlRequest x = url;
         urlRepository.save(url);
         return url.getUrl();
+    }
+
+    @GetMapping("/analyze")
+    public List<UrlRequest> getUrl() {
+        return urlRepository.findAll();
     }
 }
