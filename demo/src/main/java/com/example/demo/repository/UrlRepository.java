59e0477ba0acc3df030890911125f660ff289758
Dattebayo <angcsherpa01@gmail.com>
2026-01-01 12:03:16 -0500

    Removing UrlRepository.java

--- demo/src/main/java/com/example/demo/repository/UrlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.UrlRequest;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UrlRepository extends JpaRepository<UrlRequest, Long> {
-}
